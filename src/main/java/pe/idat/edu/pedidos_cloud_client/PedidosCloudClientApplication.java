2b03e13dae2a8154d3e9ab90883772f749de3b4a
JorgeRTorres <jorgetorres.0996@gmail.com>
2025-02-23 10:14:10 -0500

    proy-pedidos

---
+++ src/main/java/pe/idat/edu/pedidos_cloud_client/PedidosCloudClientApplication.java
@@ -0,0 +1,13 @@
+package pe.idat.edu.pedidos_cloud_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosCloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosCloudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/idat/edu/pedidos_cloud_client/PedidosCloudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat.edu.pedidos_cloud_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosCloudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
